6b3b9432105b195c96b69485f0004921b839f918
peterantonarosjr <antonaros.peter.227@gmail.com>
2022-05-04 23:25:25 -0400

    hw09 update

--- hw09/hw09_SQL.sql
+++ hw09/hw09_SQL.sql
@@ -9,7 +9,7 @@ select xtb."primarytitle", xtr."averagerating"
 	from xf_title_basic xtb
 full outer join xf_title_ratings xtr
 	on xtb."tconst" = xtr."tconst"
-where xtb."genres" = '{Comedy}' and xtb."titletype" = 'short'
+where xtb."genres" @> '{Comedy}' and xtb."titletype" = 'short'
 	and xtr."averagerating" is not null
 	and xtb."startyear" between 2000 and 2010
 order by xtr."averagerating" desc
